557176b3dd03a085b60d57f484ab6f6a05fbbe97
김지용 <wldyddkssud@naver.com>
2023-02-07 14:28:59 +0900

    feat: 프론트 배포서버와 연결 시도중

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const User = require("../schemas/user");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = `hi`;
 
+let tokenObject = {}; // Refresh Token을 저장할 Object
+
 router.post("/signup", async (req, res) => {
   console.log(req.body);
   const { userInfo } = req.body;
@@ -35,9 +37,54 @@ router.post("/login", async (req, res) => {
   if (!existUser || existUser.password !== password)
     return res.status(400).send("이메일 또는 패스워드가 틀렸습니다.");
 
-  const token = jwt.sign({ email, password }, SECRET_KEY, { expiresIn: "7s" });
+  const accessToken = createAccessToken(email);
+  const refreshToken = createRefreshToken();
 
-  return res.status(200).send(token);
+  tokenObject[refreshToken] = email;
+
+  return res
+    .status(200)
+    .send({ accessToken, refreshToken, message: "로그인 성공!" });
 });
+
+//? =====================  액세스토큰 발급  =====================
+function createAccessToken(id) {
+  const accessToken = jwt.sign({ id: id }, SECRET_KEY, { expiresIn: "20s" });
+  return accessToken;
+}
+//? =====================  리프레쉬토큰 발급  =====================
+function createRefreshToken() {
+  const refreshToken = jwt.sign({}, SECRET_KEY, { expiresIn: "1m" });
+  return refreshToken;
+}
+//? =====================  액세스토큰 검증  =====================
+function validateAccessToken(accessToken) {
+  try {
+    jwt.verify(accessToken, SECRET_KEY);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+//? =====================  리프레쉬토큰 검증  =====================
+function validateRefreshToken(refreshToken) {
+  try {
+    jwt.verify(refreshToken, SECRET_KEY);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+//? =====================  액세스토큰 페이로드  =====================
+function getAccessTokenPayload(accessToken) {
+  try {
+    const payload = jwt.verify(accessToken, SECRET_KEY);
+    return payload;
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = router;
 
 module.exports = router;
