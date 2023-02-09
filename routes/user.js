3224bc6250e9d6d860e301265ed2641174996f00
김지용 <wldyddkssud@naver.com>
2023-02-09 14:45:07 +0900

    notice: 배포서버에서는 와이리 안되는걸까

--- routes/user.js
+++ routes/user.js
@@ -208,12 +208,10 @@ router.post("/kakao", async (req, res) => {
 
   const baseUrl = "https://kauth.kakao.com/oauth/token";
 
-   const client_id= "6ad4090f0f6da30b4f468e9d81481e0e",
-    // grant_type= "authorization_code",
-   const  redirect_uri= "https://tax-back-transfer.vercel.app/kakao/auth",
-    // redirect_uri: "http://localhost:3000/kakao/auth",
-
-
+  const client_id = "6ad4090f0f6da30b4f468e9d81481e0e";
+  // grant_type= "authorization_code",
+  const redirect_uri = "https://tax-back-transfer.vercel.app/kakao/auth";
+  // redirect_uri: "http://localhost:3000/kakao/auth",
 
   const finalUrl = `${baseUrl}?client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`;
   console.log(finalUrl);
